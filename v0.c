08358bdd011012aaee5c4d5ed4a143436b6a8216
Stavrenas <stavrina25@gmail.com>
2021-09-23 22:33:39 +0300

    help me

--- v0.c
+++ v0.c
@@ -26,37 +26,28 @@ int main(int argc, char **argv)
     BlockedMatrix *blockA = (BlockedMatrix *)malloc(sizeof(BlockedMatrix));
     BlockedMatrix *blockB = (BlockedMatrix *)malloc(sizeof(BlockedMatrix));
     BlockedMatrix *blockC = (BlockedMatrix *)malloc(sizeof(BlockedMatrix));
-   
+    BlockedMatrix *blockResult = (BlockedMatrix *)malloc(sizeof(BlockedMatrix));
+
     char filenameA[] = "12.mtx";
     char filenameB[] = "12.mtx";
 
     readMatrix(filenameA, A);
     readMatrix(filenameB, B);
 
-    uint32_t blockSize = 100;
-
-    struct timeval start = tic();
-
-    blockMatrix(A, 3, blockA);
-    blockMatrix(B, 3, blockB);
+    blockMatrix(A, 4, blockA);
+    blockMatrix(B, 4, blockB);
 
     blockBMM(blockA, blockB, blockC);
 
-    printf("Time for BlockBMM: %f\n", toc(start));
+    cscBMM(A, B, C);
+    blockMatrix(C, 4, blockResult);
 
-    printBlockedMatrix(blockA);
+    printf("====BlockBMM Result====\n");
+    printBlockedMatrix(blockC);
+    printf("====BMM Result====\n\n");
+    printBlockedMatrix(blockResult);
 
-
-    //printMatrix(C);
-
-    // cscBMM2(A, B, C);
-
-    // printf("Time for mult: %f\n", toc(start));
-
-    // printMatrix(C);
     //saveMatrix(C, "mycielskianPARALLEL.txt");
-
-    // Blocking algorithms: BCSR or CSB
 
     //printf("Hello world from processor %s, rank %d out of %d processors\n",processor_name, world_rank, world_size);
     MPI_Finalize();
@@ -84,55 +75,86 @@ void blockBMM(BlockedMatrix *A, BlockedMatrix *B, BlockedMatrix *C)
     {
         for (uint32_t blockX = 1; blockX <= maxBlocks; blockX++)
         {
-            uint32_t offset = (blockY - 1) * maxBlocks + blockX;
-
+            printf("Row %d Col %d\n", blockY, blockX);
             //Create block: Cp,q (p = BlockY, q = BlockX)
 
             Matrix *block = (Matrix *)malloc(sizeof(Matrix));
-            Matrix *result = (Matrix *)malloc(sizeof(Matrix));
+            Matrix *result = (Matrix *)malloc(sizeof(Matrix)); //used for mult
+
+            block->csc_elem = (uint32_t *)malloc((block->size + 1) * sizeof(uint32_t));
+            block->size = A->list[0]->size; //get blocksize
+
+            //initialize block
+            for (int i = 0; i < block->size + 1; i++)
+                block->csc_elem[i] = 0;
 
             uint32_t indexA, indexB; //find indexes of Ap1 and B1q
-            printf("Before find index\n");
-            for (int i = 0; i < maxBlocks; i++)
+
+            for (int i = 1; i <= maxBlocks; i++)
             {
                 indexA = findIndex(A, (blockY - 1) * maxBlocks + i);
                 if (indexA != -1)
                     break; //stop when we find the first nonzero block in row p
             }
-            printf("After find index\n");
-            for (int i = 0; i < maxBlocks; i++)
+            //printf("After find index\n");
+            for (int i = 1; i <= maxBlocks; i++)
             {
-                indexB = findIndex(B, maxBlocks * i + blockX);
+                indexB = findIndex(B, maxBlocks * (i - 1) + blockX);
                 if (indexB != -1)
                     break; //stop when we find the first nonzero block in column q
             }
 
             printf("indexA is %d and indexB is %d\n", indexA, indexB);
-
             uint32_t blocksAdded = 0;
+
             for (int s = 1; s <= maxBlocks; s++)
             {
-                //check if the blocks match
+                if (indexA == -1 || indexB == -1)
+                    break;
+
                 uint32_t offsetA = A->offsets[indexA];
                 uint32_t offsetB = B->offsets[indexB];
+
                 printf("OffsetA is %d and OffsetB is %d\n", offsetA, offsetB);
-                if (offsetA % maxBlocks == offsetB / maxBlocks)
+
+                //break if blocks are out of the desired row/col
+                if (offsetA > blockY * maxBlocks || offsetB % maxBlocks > blockX)
+                    break;
+                //or if we run out of blocks
+                if (indexB > B->totalBlocks || indexA > A->totalBlocks)
+                    break;
+
+                //check if the blocks match
+                if (offsetA % maxBlocks == floor((offsetB-1) / maxBlocks) + 1 )
                 {
+                    printf("s=%d\n", s);
+
+                    printf("****Multipling A: %d with B: %d ****\n", offsetA, offsetB);
+                    printf("A\n");
+                    printMatrix(A->list[indexA]);
+                    printf("B\n");
+                    printMatrix(B->list[indexB]);
+
                     cscBMM2(A->list[indexA], B->list[indexB], result);
+                    printf("result\n");
+                    printMatrix(result);
+                    printf("Block\n");
+                    printMatrix(block);
                     addMatrix(result, block, block);
+                    printf("Block\n");
+                    printMatrix(block);
+
                     indexB++;
                     indexA++;
                     blocksAdded = 1;
                 }
-                else if (offsetA % maxBlocks > offsetB / maxBlocks)
+                else if (offsetA % maxBlocks > floor(offsetB / maxBlocks) + 1)
                     indexB++;
 
-                else if (offsetA % maxBlocks < offsetB / maxBlocks)
+                else if (offsetA % maxBlocks < floor(offsetB / maxBlocks) + 1)
                     indexA++;
-
-                if (offsetA > blockY * maxBlocks || offsetB % maxBlocks > blockX)
-                    break;
             }
+
             if (blocksAdded == 1)
             {
                 C->list[totalBlocks] = block;
@@ -145,9 +167,9 @@ void blockBMM(BlockedMatrix *A, BlockedMatrix *B, BlockedMatrix *C)
                     C->offsets = realloc(C->offsets, size * sizeof(uint32_t *));
                 }
             }
+            //free(result);
         }
     }
-
     C->size = A->size;
     C->totalBlocks = totalBlocks;
 }
