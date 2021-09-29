4a65d6c8dc59e064580a0f4c1f09866383c3cad8
Stavrenas <stavrina25@gmail.com>
2021-09-29 18:38:01 +0300

    add multBlockedMPI

--- utilities.c
+++ utilities.c
@@ -817,3 +817,98 @@ void multBlockedMatrix(BlockedMatrix *A, BlockedMatrix *B, BlockedMatrix *C)
     C->blockSize = A->blockSize;
     C->totalBlocks = totalBlocks;
 }
+
+void multBlockedMatrixMPI(BlockedMatrix *A, BlockedMatrix *B, BlockedMatrix *C,  uint32_t *rows, uint32_t rows_size)
+{
+    uint32_t size, totalBlocks, maxBlocks, blockSize;
+
+    blockSize = A->list[0]->size;
+    maxBlocks = floor(A->size / blockSize) + 1;
+    size = 1;
+    totalBlocks = 0;
+    
+    //initialize result matrix
+    C->list = (Matrix **)malloc(size * sizeof(Matrix *));
+    C->offsets = (uint32_t *)malloc(size * sizeof(uint32_t));
+    C->row_ptr = (uint32_t *)malloc(maxBlocks * sizeof(uint32_t));
+    for (int i = 0; i <= (rows_size-1); i++)
+    {
+        uint32_t blockY = rows[i]+1;
+        C->row_ptr[blockY - 1] = totalBlocks;
+        for (uint32_t blockX = 1; blockX <= maxBlocks; blockX++)
+        {
+            //Create block: Cp,q (p = BlockY, q = BlockX)
+            Matrix *block = (Matrix *)malloc(sizeof(Matrix));
+            Matrix *result = (Matrix *)malloc(sizeof(Matrix)); //used for mult
+            //initialize block
+            block->size = blockSize;
+            block->csc_elem = (uint32_t *)malloc((blockSize + 1) * sizeof(uint32_t));
+            block->csc_idx = (uint32_t *)malloc((0) * sizeof(uint32_t));
+            for (int i = 0; i <= blockSize; i++)
+                block->csc_elem[i] = 0;
+            //find indexes of Ap1 and B1q
+            uint32_t indexA, indexB;
+            for (int i = 1; i <= maxBlocks; i++)
+            {
+                indexA = findIndex(A, (blockY - 1) * maxBlocks + i);
+                if (indexA != -1)
+                    break; //stop when we find the first nonzero block in row p
+            }
+            for (int i = 1; i <= maxBlocks; i++)
+            {
+                indexB = findIndex(B, maxBlocks * (i - 1) + blockX);
+                if (indexB != -1)
+                    break; //stop when we find the first nonzero block in column q
+            }
+            //maxBlocks is the maximum number of mults for Cp,q. Variable s is not used
+            for (int s = 1; s <= maxBlocks; s++)
+            {
+                //if either block does not exist
+                if (indexA == -1 || indexB == -1)
+                    break;
+                uint32_t offsetA = A->offsets[indexA];
+                uint32_t offsetB = B->offsets[indexB];
+                //break if blocks are out of the desired row/col
+                if (offsetA > blockY * maxBlocks || offsetB % maxBlocks > blockX)
+                    break;
+                //or if we run out of blocks
+                if (indexB > B->totalBlocks || indexA > A->totalBlocks)
+                    break;
+                //check if the blocks match
+                uint32_t sA = (offsetA - 1) % maxBlocks;
+                uint32_t sB = floor((offsetB - 1) / maxBlocks);
+                //printf("sA %d, sB %d, offsetA %d, offsetB %d\n",sA, sB, offsetA,offsetB);
+                if (sA == sB)
+                {
+                    //printf("Mult %d with %d\n",offsetA,offsetB);
+                    multMatrix2(A->list[indexA], B->list[indexB], result);
+                    addMatrix(result, block, block);
+                    //find block Bsq
+                    for (int i = 1; i <= maxBlocks; i++)
+                    {
+                        indexB = findIndex(B, offsetB + maxBlocks*i);
+                       if (indexB != -1)
+                            break;
+                    }
+                    indexA++; //go to the next block in the same line of A
+                }
+                else if (sA > sB)
+                {
+                    //find block Bsq
+                    for (int i = 1; i <= maxBlocks; i++)
+                    {
+                        indexB = findIndex(B, offsetB + maxBlocks*i);
+                        if (indexB != -1)
+                            break;
+                    }
+                }
+
+                free(result); //result matrix will not be needed in the future, counter to "block" matrix
+            }
+        }
+
+        C->size = A->size;
+        C->blockSize = A->blockSize;
+        C->totalBlocks = totalBlocks;
+    }
+}
