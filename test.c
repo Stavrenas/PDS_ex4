59f3fb6ded0c5f7ea51495035d04de0770771790
Stavrenas <stavrina25@gmail.com>
2021-10-01 02:45:06 +0300

    update tester

--- test.c
+++ test.c
@@ -10,22 +10,31 @@
 int main(int argc, char **argv)
 {
 
-    printf("\n");
-    char filename[] = "12.mtx";
-    uint32_t *csc_row, *csc_col;
-    int n;
+    Matrix *A = (Matrix *)malloc(sizeof(Matrix));
+    Matrix *B = (Matrix *)malloc(sizeof(Matrix));
+    Matrix *C = (Matrix *)malloc(sizeof(Matrix));
 
-    readMatrix(csc_row, csc_col, &n, filename);
+    BlockedMatrix *blockA = (BlockedMatrix *)malloc(sizeof(BlockedMatrix));
+    BlockedMatrix *blockB = (BlockedMatrix *)malloc(sizeof(BlockedMatrix));
+    BlockedMatrix *blockC = (BlockedMatrix *)malloc(sizeof(BlockedMatrix));
 
-    printf("TEST is %d\n", n);
+    // BlockedMatrix *blockResult = (BlockedMatrix *)malloc(sizeof(BlockedMatrix));
 
-    for (uint32_t i = 1; i <= n; i++)
-    {
-        for (uint32_t i1 = csc_col[i - 1]; i1 < csc_col[i]; i1++)
-        {
-            printf("%d ",csc_row[i1]);
-        }
-        printf("\n");
-    }
+    char matrix[] = "12";
+    char *filenameA = (char *)malloc(25 * sizeof(char));
+    char *filenameB = (char *)malloc(25 * sizeof(char));
+    char *name = (char *)malloc(25 * sizeof(char));
+    sprintf(filenameA, "%s.mtx", matrix);
+    sprintf(filenameB, "%s.mtx", matrix);
 
-  }
+    readMatrix(filenameA, A);
+    readMatrix(filenameB, B);
+
+    int blocksize = 4;
+    printf("Elements before blocking: %d\n",A->csc_elem[A->size]);
+    blockMatrix(A, blocksize, blockA);
+    blockMatrix(B, blocksize, blockB);
+    unblockMatrix2(blockA,C);
+    printMatrix(C);
+    printMatrix(A); 
+}
