051fd3475757f4dc710350b1882bfd02f1a6b2b9
AntoniosOurdas <45215986+AntoniosOurdas@users.noreply.github.com>
2021-10-05 14:57:14 +0300

    Included all versions to script.

--- script.sh
+++ script.sh
@@ -1,24 +1,51 @@
 #!/bin/bash
 # This is a script to measure perfomance
-matrices=(50 100 1000);
+# Define matrices to be tested
+mtx=(50 100 1000);
+# Define block size for each matrix
 blockSizes=(4 10 50);
+# Define number of proccesses to be tested in v3 (MPI)
 np=(1 2 4);
 
-echo -n "" > times.csv;
-echo -n "matrix" >> times.csv;
+# v0
+echo "matrix;1" > times.csv;
+for m in "${mtx[@]}"; do
+    echo -n "$m;" >> times.csv;
+    ./v0 $m >> times.csv;
+done
 
+echo "" >> times.csv
+
+# v1
+echo "matrix;1" >> times.csv;
+for m in "${mtx[@]}"; do
+    echo -n "$m;" >> times.csv;
+    ./v1 $m >> times.csv;
+done
+
+echo "" >> times.csv
+
+# v2
+echo "matrix;1" >> times.csv;
+for m in "${!mtx[@]}"; do
+    echo -n "$m;" >> times.csv;
+    ./v2 ${mtx[m]} ${blockSizes[m]} >> times.csv;
+done
+
+echo "" >> times.csv;
+
+# v3
+echo -n "matrix" >> times.csv;
 for n in "${np[@]}"; do
     echo -n ";"$n"" >> times.csv;
 done
 echo "" >> times.csv;
 
-for m in "${!matrices[@]}"; do
-    echo -n "${matrices[m]}" >> times.csv;
-
+for m in "${!mtx[@]}"; do
+    echo -n "${mtx[m]}" >> times.csv
     for n in "${np[@]}"; do
         echo -n ";" >> times.csv;
-        mpirun -np "$n" ./v0 "${matrices[m]}" "${blockSizes[m]}" >> times.csv;
+        mpirun -np "$n" ./v3 "${mtx[m]}" "${blockSizes[m]}" >> times.csv;
     done
-
     echo "" >> times.csv;
 done
